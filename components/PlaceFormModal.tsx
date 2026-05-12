1ab09cba285fafefd3ef6703c14aab910fe3226b
Ken Yeh <ken0131@gmail.com>
2026-05-13 02:19:40 +0800

    fix typo

--- components/PlaceFormModal.tsx
+++ components/PlaceFormModal.tsx
@@ -464,7 +464,7 @@ export function PlaceFormModal({
             </select>
           </label>
 
-          {formValues.status === "viwantToReturn" ? (
+          {formValues.status === "wantToReturn" ? (
             <label className="block text-sm">
               <span className={labelTitleClassName}>完成日期</span>
 
